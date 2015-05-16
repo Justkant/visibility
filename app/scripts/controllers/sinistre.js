17e84239f9c0b10ed3829260f08551f2fe143dd5
Julien Zhang <jzepitech@gmail.com>
2015-05-16 20:39:56 +0200

    fix

--- app/scripts/controllers/sinistre.js
+++ app/scripts/controllers/sinistre.js
@@ -9,93 +9,95 @@
  */
 angular.module('visibilityApp')
     .controller('SinistreCtrl', function ($scope, $routeParams, $location, user, Ref, $firebaseObject) {
-            var usr = $firebaseObject(Ref.child('users/' + user.uid));
-            usr.$loaded().then(function (data) {
-                $scope.user = data;
-                $scope.x = {
-                    step: data.type + '0'
-                };
-            });
-
-            var sinistre = $firebaseObject(Ref.child('sinistres/' + $routeParams.id));
-            sinistre.$loaded().then(function (data) {
-                $scope.sinistre = data;
-            });
-
-            $scope.triggerUpload = function (index) {
-                angular.element('#my-file' + index).trigger('click');
+        var usr = $firebaseObject(Ref.child('users/' + user.uid));
+        usr.$loaded().then(function (data) {
+            $scope.user = data;
+            $scope.x = {
+                step: data.type + '0'
             };
-            $scope.triggerPictureUpload = function () {
-                console.log('lol');
-                angular.element('#my-picture').trigger('click');
-            };
+        });
 
-            $scope.addFile = function (files, index) {
-                if (files && files[0]) {
-                    var reader = new FileReader();
-                    var dataURL;
-                    reader.onload = function (e) {
-                        dataURL = e.target.result;
-                        $scope.sinistre.expertise.docs[index].file = {
-                            name: files[0].name,
-                            data: dataURL
-                        };
-                        $scope.sinistre.$save();
+        var sinistre = $firebaseObject(Ref.child('sinistres/' + $routeParams.id));
+        sinistre.$loaded().then(function (data) {
+            $scope.sinistre = data;
+        });
+
+        $scope.triggerUpload = function (index) {
+            angular.element('#my-file' + index).trigger('click');
+        };
+        $scope.triggerPictureUpload = function () {
+            console.log('lol');
+            angular.element('#my-picture').trigger('click');
+        };
+
+        $scope.addFile = function (files, index) {
+            if (files && files[0]) {
+                var reader = new FileReader();
+                var dataURL;
+                reader.onload = function (e) {
+                    dataURL = e.target.result;
+                    $scope.sinistre.expertise.docs[index].file = {
+                        name: files[0].name,
+                        data: dataURL
                     };
-                    reader.readAsDataURL(files[0]);
-                }
-            };
+                    $scope.sinistre.$save();
+                };
+                reader.readAsDataURL(files[0]);
+            }
+        };
 
-            $scope.seeFile = function (index) {
+        $scope.seeFile = function (index) {
 
-            };
+        };
 
-            $scope.dlFile = function (index) {
+        $scope.dlFile = function (index) {
 
-            };
+        };
 
+        $scope.newReparationName = '';
+        $scope.createNewReparation = function (reparationName)  {
+            $scope.sinistre.reparation.push({
+                name: reparationName,
+                validated: false
+            });
+            $scope.sinistre.$save();
             $scope.newReparationName = '';
-            $scope.createNewReparation = function (reparationName)  {
-                $scope.sinistre.reparation.push({
-                    name: reparationName,
-                    validated: false
+        };
+
+        $scope.addPicture = function (files) {
+            if (files && files[0]) {
+                var reader = new FileReader();
+                var dataURL;
+                reader.onload = function (e) {
+                    dataURL = e.target.result;
+                    if (!$scope.sinistre.pictures) {
+                        $scope.sinistre.pictures = [];
+                        $scope.sinistre.$save();
+                    }
+
+                    $scope.sinistre.pictures.push({
+                        name: files[0].name,
+                        data: dataURL
+                    });
+                    $scope.sinistre.$save();
+                };
+                reader.readAsDataURL(files[0]);
+            }
+        };
+
+        $scope.chatOpen = false;
+        $scope.msg = '';
+        $scope.pushMessage = function () {
+            if ($scope.msg.length > 0) {
+                if (!$scope.sinistre.messages) {
+                    $scope.sinistre.messages = [];
+                }
+                $scope.sinistre.messages.push({
+                    name: $scope.user.name,
+                    msg: $scope.msg
                 });
                 $scope.sinistre.$save();
-                $scope.newReparationName = '';
-            };
-
-            $scope.addPicture = function (files) {
-                if (files && files[0]) {
-                    var reader = new FileReader();
-                    var dataURL;
-                    reader.onload = function (e) {
-                        dataURL = e.target.result;
-                        if (!$scope.sinistre.pictures) {
-                            $scope.sinistre.pictures = [];
-                            $scope.sinistre.$save();
-                        }
-
-                        $scope.sinistre.pictures.push({
-                            name: files[0].name,
-                            data: dataURL
-                        });
-                        $scope.sinistre.$save();
-                    };
-                    reader.readAsDataURL(files[0]);
-
-                    $scope.chatOpen = false;
-                    $scope.msg = '';
-                    $scope.pushMessage = function () {
-                        if ($scope.msg.length > 0) {
-                            if (!$scope.sinistre.messages) {
-                                $scope.sinistre.messages = [];
-                            }
-                            $scope.sinistre.messages.push({
-                                name: $scope.user.name,
-                                msg: $scope.msg
-                            });
-                            $scope.sinistre.$save();
-                            $scope.msg = '';
-                        }
-                    };
-                });
+                $scope.msg = '';
+            }
+        };
+    });
